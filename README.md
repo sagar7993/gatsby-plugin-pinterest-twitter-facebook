cd52a49d9ce5d712e9ec11b05ea81441cea2d93c
Sagar Jain <sagar@zitto.com>
2019-09-20 14:58:51 +0530

    feat: added customizations for twitter and facebook

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # gatsby-plugin-pinterest-twitter-facebook
-Load the Pinterest, Twitter and Facebook JavaScript to let users save images to their Pinterest boards on image hover, load Twitter timeline, and load Facebook like button
+Load the Pinterest, Twitter and Facebook JavaScript to let users save images to their Pinterest boards on image hover, load Twitter timeline and follow button, and load Facebook timeline and like button
 
 [![npm](https://img.shields.io/npm/dt/gatsby-plugin-pinterest-twitter-facebook.svg)](https://www.npmjs.com/package/gatsby-plugin-pinterest-twitter-facebook)
 [![npm](https://img.shields.io/npm/v/gatsby-plugin-pinterest-twitter-facebook.svg)](https://www.npmjs.com/package/gatsby-plugin-pinterest-twitter-facebook)
 [![NpmLicense](https://img.shields.io/npm/l/gatsby-plugin-pinterest-twitter-facebook.svg)](https://opensource.org/licenses/MIT)
-[![GitHub issues](https://img.shields.io/github/issues-raw/sagar7993/gatsby-plugin-pinterest.svg)](https://github.com/sagar7993/gatsby-plugin-pinterest-twitter-facebook/issues)
+[![GitHub issues](https://img.shields.io/github/issues-raw/sagar7993/gatsby-plugin-pinterest-twitter-facebook.svg)](https://github.com/sagar7993/gatsby-plugin-pinterest-twitter-facebook/issues)
 
 ## Install
 
@@ -21,14 +21,64 @@ plugins: [
   {
     resolve: `gatsby-plugin-pinterest-twitter-facebook`,
     options: {
-      // Set to true to display a bigger button
-      tall: true, // default
-      // Set to true to hide the text and display only a round P button
-      round: false // default
+      pinterest: {
+        // Set this to false if you want to disable pinterest
+        enable: true, // default
+        // Set this to true to display a larger size button
+        tall: true, // default
+        // Set to true to hide the pinterest text and display only a round P button
+        round: false // default
+      }
+      twitter: {
+        // Set this to false if you want to disable twitter
+        enable: true, // default
+        // Set this to the querySelector string of the container element where you want to display twitter timeline and follow button
+        // If you do not define this, twitter timeline and follow button will not work
+        // Fox example: `.twitter-container` or `#.twitter-timeline`
+        containerSelector: null,
+        // Set this to the twitter handle you want to use
+        // Do not include `@` symbol here
+        // If you do not define this, twitter timeline and follow button will not work
+        handle: null,
+        // Set this to false if you do not want to show follow button
+        showFollowButton: true // default
+        // Set this to false if you do not want to show timeline
+        showTimeline: true // default
+      }
+      facebook: {
+        // Set this to false if you want to disable facebook
+        enable: true, // default
+        // Set this to the querySelector string of the container element where you want to display facebook timeline and like button
+        // If you do not define this, facebook timeline and like button will not work
+        // Fox example: `.facebook-container` or `#.facebook-timeline`
+        containerSelector: null,
+        // Set this to the facebook profile or facebook page name you want to use
+        // If you do not define this, facebook timeline and like button will not work
+        // Do not include `@` symbol here
+        profile: null,
+        // Set this to false if you do not want to show like button
+        showLikeButton: true // default
+        // Set this to false if you do not want to show timeline
+        showTimeline: true // default
+      }
     }
   }
 ];
 ```
+
+## Pinterest
+
+Check [Pinterest Docs](https://developers.pinterest.com/docs/widgets/save/) for more customization options and examples.
+
+### Note
+
+If you don't want people to save things to Pinterest from your website, just paste this code into the <head> section of any page on your site:
+
+```<meta name="pinterest" content="nopin" />```
+
+When someone tries to save things to Pinterest from your site, they'll see: "This site doesn't allow saving to Pinterest. Please contact the owner with any questions. Thanks for visiting!"
+
+Visit [Pinterest Docs](https://help.pinterest.com/en/business/article/prevent-saves-to-pinterest-from-your-site) for more customization options and examples.
 
 ## Contribute
 
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,94 @@
-const injectPinterestScript = (tall, round) => {
-  function addJS() {
-    const s = document.createElement(`script`);
-    s.type = `text/javascript`;
-    s.setAttribute("async", true);
-    s.setAttribute("defer", true);
-      s.setAttribute("data-pin-hover", "true");
-    if(tall) {
-      s.setAttribute("data-pin-tall", "true");
-    }
-    if(round) {
-      s.setAttribute("data-pin-round", "true");
-    }
-    s.setAttribute("src", "https://assets.pinterest.com/js/pinit.js");
-    document.getElementsByTagName(`head`)[0].appendChild(s);
-  }
-  addJS();
+const PINTEREST_SCRIPT_URL = 'https://assets.pinterest.com/js/pinit.js';
+const TWITTER_SCRIPT_URL = 'https://platform.twitter.com/widgets.js';
+const FACEBOOK_SCRIPT_URL = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0';
+
+const injectPreloadLink = (url) => {
+	try {
+		const link = document.createElement('link');
+		link.rel = 'preload';
+		link.as = 'script';
+		link.href = url;
+		document.head.appendChild(link);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const injectPinterestScript = (pinterestOptions) => {
+	try {
+		if (pinterestOptions && pinterestOptions.enable) {
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.setAttribute('async', 'async');
+			script.setAttribute('defer', 'defer');
+			script.setAttribute('data-pin-hover', 'true');
+			if (pinterestOptions.tall) {
+				script.setAttribute('data-pin-tall', 'true');
+			}
+			if (pinterestOptions.round) {
+				script.setAttribute('data-pin-round', 'true');
+			}
+			script.setAttribute('src', PINTEREST_SCRIPT_URL);
+			injectPreloadLink(PINTEREST_SCRIPT_URL);
+			document.getElementsByTagName('head')[0].appendChild(s);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const injectTwitterScript = (twitterOptions) => {
+	try {
+		if (twitterOptions && twitterOptions.enable && twitterOptions.containerSelector && document.querySelector(twitterOptions.containerSelector) && twitterOptions.handle) {
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.setAttribute('async', 'async');
+			script.setAttribute('defer', 'defer');
+			script.setAttribute('src', TWITTER_SCRIPT_URL);
+			injectPreloadLink(TWITTER_SCRIPT_URL);
+			const twitterContainer = document.querySelector(twitterOptions.containerSelector);
+			const twitterFollowButton = `<a href="https://twitter.com/${twitterOptions.handle}?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="true">Follow @${twitterOptions.handle}</a>`;
+			const twitterTimeline = `<a class="twitter-timeline" href="https://twitter.com/${twitterOptions.handle}" data-chrome="noheader nofooter noborders noscrollbar transparent" data-tweet-limit="1">`;
+			twitterContainer.appendChild(twitterFollowButton);
+			twitterContainer.appendChild(twitterTimeline);
+			document.getElementsByTagName('head')[0].appendChild(s);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const injectFacebookScript = (facebookOptions) => {
+	try {
+		if (facebookOptions && facebookOptions.enable && facebookOptions.containerSelector && facebookOptions.profile) {
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.setAttribute('async', 'async');
+			script.setAttribute('defer', 'defer');
+			script.setAttribute('src', FACEBOOK_SCRIPT_URL);
+			injectPreloadLink(FACEBOOK_SCRIPT_URL);
+			const facebookLikeButton = `<a href="https://facebook.com/${facebookOptions.profile}?ref_src=twsrc%5Etfw" class="facebook-follow-button" data-show-count="true">Follow @${facebookOptions.profile}</a>`;
+			const facebookTimeline = `<a class="facebook-timeline" href="https://facebook.com/${facebookOptions.profile}" data-chrome="noheader nofooter noborders noscrollbar transparent" data-tweet-limit="1">`;
+			document.head.appendChild(res);
+			document.getElementsByTagName('head')[0].appendChild(s);
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 exports.onClientEntry = (_args, pluginOptions) => {
-  const {
-    tall = true,
-    round = false,
-  } = pluginOptions;
-  injectPinterestScript(tall, round);
+	try {
+		if (pluginOptions && pluginOptions.pinterest && pluginOptions.pinterest.enable) {
+			injectPinterestScript(pluginOptions.pinterest);
+		}
+		if (pluginOptions && pluginOptions.twitter && pluginOptions.twitter.enable) {
+			injectTwitterScript(pluginOptions.twitter);
+		}
+		if (pluginOptions && pluginOptions.facebook && pluginOptions.facebook.enable) {
+			injectFacebookScript(pluginOptions.facebook);
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
